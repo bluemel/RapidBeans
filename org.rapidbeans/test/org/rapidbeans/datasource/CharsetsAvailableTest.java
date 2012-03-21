d760b18dbcc8d744c28b3be9ddae969b5f9a4922
unknown <Bluemel1.Martin@l80069197.viola.local>
2012-03-21 17:58:11 +0100

    new test for CharsetsAvailable

---
+++ org.rapidbeans/test/org/rapidbeans/datasource/CharsetsAvailableTest.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package org.rapidbeans.datasource;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * @author Bluemel1.Martin
+ *
+ */
+public class CharsetsAvailableTest {
+
+	@Test
+	public void test() {
+		CharsetsAvailable utf8 = CharsetsAvailable.UTF_8;
+		assertSame(utf8, CharsetsAvailable.getInstance("UTF-8"));
+	}
+}
